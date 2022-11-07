7646011f72b74e44b352fdd525344225ab3912b1
Laurentiu Ciobanu <ciobanulaur@gmail.com>
2022-11-07 01:30:45 +0000

    opeartor overloading

--- demos/showcase/dist/index.js
+++ demos/showcase/dist/index.js
@@ -9,19 +9,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-class Test extends model_1.Repository {
+class Test {
 }
+class Sum {
+}
+class Vec2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    print() {
+        console.log(`(${this.x}, ${this.y})`);
+    }
+    plusEquals(other) {
+        if (typeof other === "number") {
+            this.x += other;
+            this.y += other;
+        }
+        else {
+            this.x += other.x;
+            this.y += other.y;
+        }
+    }
+    plus(other) {
+        return new Vec2(this.x + other.x, this.y + other.y);
+    }
+    lessThan(other) {
+        return this.x < other.x && this.y < other.y;
+    }
+}
+const ENDL = "\n";
+class Stdout {
+    write(str) {
+        process.stdout.write(str);
+    }
+    shiftLeft(str) {
+        this.write(str.toString());
+        return this;
+    }
+}
+const cout = new Stdout();
+((cout.shiftLeft("hello")).shiftLeft(" world")).shiftLeft(ENDL);
+let vec1 = new Vec2(1, 2);
+const vec2 = new Vec2(2, 3);
+const sum = (vec1.plus(vec2)).plus(new Vec2(1, 1));
+vec1.plusEquals(vec2);
+vec1.plusEquals(3);
+if (vec1.lessThan(vec2)) {
+    console.log("vec1 is smaller than vec2");
+}
+sum.print();
 function t() {
     return __awaiter(this, void 0, void 0, function* () {
         const test = new Test();
         const x = test.test();
-        const zz = Test.findOne("123");
-        zz.x;
     });
 }
 function main() {
-    const x = contextual_return_1.contextualReturn();
-    console.log("User");
+    // const x = contextualReturn!();
+    // console.log(typename!<typeof x>());
     return {};
 }
 main();
--- demos/showcase/src/index.ts
+++ demos/showcase/src/index.ts
@@ -1,28 +1,96 @@
 import { contextualReturn } from "./contextual-return";
 import { typename } from "./typename";
-import { Model, Repository } from "./model";
+import { Model } from "./model";
 
 interface User {
     z: number
 }
 
-class Test extends Repository derives Model<User> {
+class Test derives Model<User> {
     public x!: string;
 }
 
+class Sum {
+
+}
+
+class Vec2 {
+    constructor(
+        public x: number,
+        public y: number
+    ) {}
+
+    print(): void {
+        console.log(`(${this.x}, ${this.y})`);
+    }
+
+    plusEquals(other: number): void
+    plusEquals(other: Vec2): void
+    plusEquals(other: number | Vec2): void {
+        if (typeof other === "number") {
+            this.x += other;
+            this.y += other;
+        } else {
+            this.x += other.x;
+            this.y += other.y;
+        }
+    }
+
+    plus(other: Vec2): Vec2 {
+        return new Vec2(
+            this.x + other.x,
+            this.y + other.y
+        );
+    }
+
+    lessThan(other: Vec2): boolean {
+        return this.x < other.x && this.y < other.y;
+    }
+}
+
+const ENDL = "\n";
+
+class Stdout {
+    private write(str: string) {
+        process.stdout.write(str);
+    }
+
+
+    shiftLeft(str: string | number | boolean): Stdout {
+        this.write(str.toString());
+        return this;
+    }
+}
+
+
+const cout = new Stdout();
+
+cout << "hello" << " world" << ENDL;
+
+let vec1 = new Vec2(1, 2);
+const vec2 = new Vec2(2, 3);
+
+
+const sum = vec1 + vec2 + new Vec2(1, 1);
+
+vec1 += vec2;
+vec1 += 3;
+
+if(vec1 < vec2) {
+    console.log("vec1 is smaller than vec2");
+}
+
+sum.print();
 
 async function t() {
     const test = new Test();    
     const x = test.test();
-
-    const zz = Test.findOne("123");
-    zz.x
 }
 
 function main(): User {
-    const x = contextualReturn!();
+    // const x = contextualReturn!();
 
-    console.log(typename!<typeof x>());
+    // console.log(typename!<typeof x>());
 
 
     return {} as any;
